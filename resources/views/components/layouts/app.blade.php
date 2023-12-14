b1a298d433a0e878f1f8d5d2d3f00b4ee0ba277e
Matheus Mota <matheus.mat1996@gmail.com>
2023-12-14 15:23:41 -0300

    LiveMotion - V1.0

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -15,12 +15,16 @@
     <body class="flex min-h-screen">
 
         <!-- Desktop -->
-        <div class="flex-col hidden w-60 lg:flex">
-            <div class="w-full border-gray-200 shadow-sm">
+        <div class="flex-col hidden shadow-lg w-60 lg:flex">
+            <div class="w-full shadow-sm">
                 <img src="assets/img/logo.png" alt="Logo LifeMotion" class="h-16 m-auto">
             </div>
-            <nav>
-                
+            <nav class="flex flex-col gap-3 my-2">
+                <div class="flex items-center justify-center h-10 bg-teal-500 hover:bg-teal-700">
+                    <a href="" class="text-center">
+                        <span class="text-sm text-white">Avaliação Corporal</span>
+                    </a>
+                </div>
             </nav>
         </div>
 
